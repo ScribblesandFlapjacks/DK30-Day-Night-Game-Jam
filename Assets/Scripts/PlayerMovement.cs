e7707d2a61dc11d0b0d03ecb2d8f00bac97aade7
Scribbles and Flapjacks <mpsmitch@gmail.com>
2019-09-19 04:20:52 -0500

    Reworked test sprites and implemented v1 of player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Vector2 origin = new Vector2(0, 0);
+    int currentRotation = 0;
+    float circleRadius = 3.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentRotation--;
+            Debug.Log(currentRotation);
+            float rotationInRadians = Mathf.PI * currentRotation / 180;
+            Vector2 newPosition = new Vector2(circleRadius * Mathf.Sin(rotationInRadians), circleRadius * Mathf.Cos(rotationInRadians));
+            transform.position = newPosition;
+            transform.rotation = Quaternion.Euler(0,0,-currentRotation);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            currentRotation++;
+            Debug.Log(currentRotation);
+            float rotationInRadians = Mathf.PI * currentRotation / 180;
+            Vector2 newPosition = new Vector2(circleRadius * Mathf.Sin(rotationInRadians), circleRadius * Mathf.Cos(rotationInRadians));
+            transform.position = newPosition;
+            transform.rotation = Quaternion.Euler(0, 0, -currentRotation);
+        }
     }
 }
