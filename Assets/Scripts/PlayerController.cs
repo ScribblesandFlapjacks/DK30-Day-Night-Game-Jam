ff8014aa89625f4424fa72785468c779a177d257
Scribbles and Flapjacks <mpsmitch@gmail.com>
2019-09-19 17:17:43 -0500

    Refactored PlayerMovement update function into PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //cached references
+    PlayerMovement playerMovement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerMovement = FindObjectOfType<PlayerMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerMovement.MoveCounterclockwise();
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerMovement.MoveClockwise();
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,33 +7,23 @@ public class PlayerMovement : MonoBehaviour
     Vector2 origin = new Vector2(0, 0);
     int currentRotation = 0;
     float circleRadius = 3.5f;
-
-    // Start is called before the first frame update
-    void Start()
+    public void MoveCounterclockwise()
     {
-        
+        currentRotation--;
+        //Debug.Log(currentRotation);
+        float rotationInRadians = Mathf.PI * currentRotation / 180;
+        Vector2 newPosition = new Vector2(circleRadius * Mathf.Sin(rotationInRadians), circleRadius * Mathf.Cos(rotationInRadians));
+        transform.position = newPosition;
+        transform.rotation = Quaternion.Euler(0, 0, -currentRotation);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveClockwise()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            currentRotation--;
-            Debug.Log(currentRotation);
-            float rotationInRadians = Mathf.PI * currentRotation / 180;
-            Vector2 newPosition = new Vector2(circleRadius * Mathf.Sin(rotationInRadians), circleRadius * Mathf.Cos(rotationInRadians));
-            transform.position = newPosition;
-            transform.rotation = Quaternion.Euler(0,0,-currentRotation);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            currentRotation++;
-            Debug.Log(currentRotation);
-            float rotationInRadians = Mathf.PI * currentRotation / 180;
-            Vector2 newPosition = new Vector2(circleRadius * Mathf.Sin(rotationInRadians), circleRadius * Mathf.Cos(rotationInRadians));
-            transform.position = newPosition;
-            transform.rotation = Quaternion.Euler(0, 0, -currentRotation);
-        }
+        currentRotation++;
+        //Debug.Log(currentRotation);
+        float rotationInRadians = Mathf.PI * currentRotation / 180;
+        Vector2 newPosition = new Vector2(circleRadius * Mathf.Sin(rotationInRadians), circleRadius * Mathf.Cos(rotationInRadians));
+        transform.position = newPosition;
+        transform.rotation = Quaternion.Euler(0, 0, -currentRotation);
     }
 }
