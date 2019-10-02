d67b6afaccd7c6a0593b831400f9c6f986333ede
Scribbles and Flapjacks <mpsmitch@gmail.com>
2019-10-01 23:30:53 -0500

    Changed player movement to be independent of fps.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 origin = new Vector2(0, 0);
-    int currentRotation = 0;
+    [SerializeField] float playerSpeed = 50;
+    float currentRotation = 0;
     float circleRadius = 3.5f;
     public void MoveCounterclockwise()
     {
-        currentRotation--;
+        currentRotation -= Time.deltaTime * playerSpeed;
         float rotationInRadians = Mathf.PI * currentRotation / 180;
         Vector2 newPosition = new Vector2(circleRadius * Mathf.Sin(rotationInRadians), circleRadius * Mathf.Cos(rotationInRadians));
         transform.position = newPosition;
@@ -19,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void MoveClockwise()
     {
-        currentRotation++;
+        currentRotation += Time.deltaTime * playerSpeed;
         float rotationInRadians = Mathf.PI * currentRotation / 180;
         Vector2 newPosition = new Vector2(circleRadius * Mathf.Sin(rotationInRadians), circleRadius * Mathf.Cos(rotationInRadians));
         transform.position = newPosition;
@@ -32,7 +33,7 @@ public class PlayerMovement : MonoBehaviour
         return transform.position;
     }
 
-    public int GetCurrentRotationDegree()
+    public float GetCurrentRotationDegree()
     {
         return currentRotation;
     }
